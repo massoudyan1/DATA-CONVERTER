80e9af3bf4bcc4c0437d23e4a6fd400f2d1dd8e4
massoudyan1 <47738720+massoudyan1@users.noreply.github.com>
2021-03-17 14:55:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+console.log("script startet");
+function reverse(s){
+    return s.split("").reverse().join("");
+}
 
+function decToBin(){
+	let num = 0.00;
+	let remainder = "";
+	let n;
+
+	num = document.getElementById("decToBin").value;
+	
+	for(;;){
+		num = num /2;
+		n = parseInt(num);
+
+		if( parseFloat(num - n) == 0) 
+			remainder += "0";
+			else 
+			remainder += 1;
+
+		console.log("n = " + n);
+		console.log("remainder = " + reverse(remainder));
+
+		if(num == 0){
+			document.getElementById("decToBinResult").innerHTML = reverse(remainder);
+			return null;
+		}
+		num = parseFloat(n);
+	}
+
+
+	
+}
