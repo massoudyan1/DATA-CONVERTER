d3025d8c2359bd83cfd5671dad164ba1476338e5
massoudyan1 <47738720+massoudyan1@users.noreply.github.com>
2021-03-22 13:07:47 +0100

    dec to hex done

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function decToBin(){
 		if( parseFloat(num - n) == 0) 
 			remainder += "0";
 			else 
-			remainder += 1;
+			remainder += "1";
 
 		console.log("n = " + n);
 		console.log("remainder = " + reverse(remainder));
@@ -28,7 +28,58 @@ function decToBin(){
 		}
 		num = parseFloat(n);
 	}
+}
 
+	function decToHex(){
+		let num = 0.00;
+		let remainder = "";
+		let n;
+	
+		num = document.getElementById("decToHex").value;
+		
+		for(;;){
+			num = num /16;
+			n = parseInt(num);
+
+			console.log("n = " + n);
+			console.log("remainder = " + reverse(remainder));
+
+
+			if(parseFloat(num - n) * 16 < 10)
+				remainder += "" + (parseFloat(num - n) * 16);
+			else {
+				switch (parseFloat(num - n) * 16) {
+					case 10:
+						remainder += "A";
+						break;
+					case 11:
+						remainder += "B";
+						break;
+					case 12:
+						remainder += "C";
+						break;
+					case 13:
+						remainder += "D";
+						break;
+					case 14:
+						remainder += "E";
+						break;
+					case 15:
+						remainder += "F";
+						break;
+						
+					default:
+						break;
+				}
+			}
+			parseFloat(num - n) * 16;
+			if(num == 0){
+				document.getElementById("decToHexResult").innerHTML = reverse(remainder);
+				return null;
+			}
+			
+			num = parseFloat(n);
+		}
 
 	
 }
